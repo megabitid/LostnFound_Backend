e5ef4dd015ed8fa66415319b88e19898f9c69b9d
Umar Sabirin <umarsabirin369@gmail.com>
2020-11-12 13:19:57 +0800

    :sparkles: Add feature admin

---
+++ app/Http/Resources/AdminResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AdminResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                => $this->id,
+            'nama'              => $this->nama,
+            'nip'               => $this->nip,
+            'email'             => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'stasiun'           => $this->stasiun,
+            'image'             => $this->image,
+            'role'              => $this->role == 1 ? "Admin" : "Reguler",
+            'token'             => $this->token
+        ];
+    }
+}
