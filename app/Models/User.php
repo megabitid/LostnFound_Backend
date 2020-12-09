c139d2716fa34f9ffb04780024b4121edfc92c19
Eugene <61174287+eugene18-art@users.noreply.github.com>
2020-12-09 18:18:15 +0800

    :pencil: Add stasiun relation to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -26,6 +25,7 @@ class User extends Authenticatable implements JWTSubject
         'password',
         'image',
         'role',
+        'stasiun_id',
         'email_verified_at',
     ];
 
@@ -67,5 +67,10 @@ class User extends Authenticatable implements JWTSubject
     public function histories()
     {
         return $this->hasMany(History::class);
+    }
+    
+    public function stasiun()
+    {
+        return $this->belongsTo('App\Models\Stasiun');
     }
 }
